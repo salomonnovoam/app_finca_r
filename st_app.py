41d723f4d53ad4b1e49a7106385bd34cc55b0ce8
salomonnovoam <52713477+salomonnovoam@users.noreply.github.com>
2023-05-31 19:12:24 -0500

    Update st_app.py

--- st_app.py
+++ st_app.py
@@ -64,7 +64,7 @@ with tab1:
     if st.button('Recolectar datos'):
         with st.spinner('Recolectando datos...'):
             data_r = executor_lasso(search_r)
-            # df_to_predict = get_processed_info(search_r)
-            # st.dataframe(df_to_predict.drop(['price'],axis=1)) 
+            df_to_predict = get_processed_info(search_r)
+            st.dataframe(df_to_predict.drop(['price'],axis=1)) 
             st.write('El precio de la propiedad debería ser', data_r) 
 
