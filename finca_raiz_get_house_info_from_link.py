3456cbb5389b154c553d4b603f7e00391d581c1d
salomonnovoam <52713477+salomonnovoam@users.noreply.github.com>
2023-05-31 20:11:23 -0500

    Update finca_raiz_get_house_info_from_link.py

--- finca_raiz_get_house_info_from_link.py
+++ finca_raiz_get_house_info_from_link.py
@@ -12,8 +12,12 @@ def get_proxy_dc()-> dict:
     """
 
 
+    username = 'brd-customer-hl_435d7aea-zone-data_center-country-in'
+    password = '8dletu9t4a7u'
+
     username =  'brd-customer-hl_435d7aea-zone-residential-country-in'
     password = 'evb8oehm4sza'
+
 
 
     port = 22225
@@ -26,6 +30,7 @@ def get_proxy_dc()-> dict:
     } 
     return proxy_handler
 
+#%%
 def get_json_from_url(link):
 
     try:
@@ -37,8 +42,8 @@ def get_json_from_url(link):
         }
 
         response = requests.get( link,
-            headers=headers, proxies = get_proxy_dc()
-                             
+            headers=headers,
+            proxies = get_proxy_dc()
         )
 
         soup = BeautifulSoup(response.text, 'lxml')
@@ -46,89 +51,10 @@ def get_json_from_url(link):
 
         discovery = soup.find('script', {'id':'__NEXT_DATA__'}).text
         print(link)
-        return json.loads(discovery)['props']['pageProps']
+        # return json.loads(discovery)['props']['pageProps']
+        return response.status_code#json.loads(discovery)['props']['pageProps']
     except Exception as e:
         print(e)
         print('-----error----')
         print(link)
-# %%
-
-
-def prepare (i):
-    try:
-        cat_sec = i['categories']['sector']
-        # print(cat_sec)
-        for j in cat_sec:
-            value = j['slug']
-            print(value)
-            i[value]=1
-    except:
-        print('a')
-        pass
-
-    try:
-        cat_int = i['categories']['interior']
-        # print(cat_int)
-        for j in cat_int:
-            value = j['slug']
-            print(value)
-            i[value]=1
-    except:
-        print('b')
-        pass
-        
-    try:
-        cat_ext = i['categories']['exterior']
-        # print(cat_ext)
-        for j in cat_ext:
-            value = j['slug']
-            print(value)
-            i[value]=1
-    except:
-        print('c')
-        pass
-
-    return i
-
-
-def get_processed_info(link):
-    print(link)
-
-    a = get_json_from_url(link)
-    print(a)
-    a_ = prepare (a)
-
-
-
-    #df = pd.json_normalize(a_)[[ 'price',
-    #'administration.price',
-    #'age.id',
-    #'age.name',
-    #'area',
-    #'baths.id',
-    #'baths.name',
-    #'client.type',
-    #'condition.id',
-    #'floor.id',
-    #'garages.id',
-    #'locations.lat',
-    #'locations.lng',
-    #'rooms.name',
-    #'segmentation.baños',
-    #'segmentation.habitaciones',
-    #'segmentation.tipo_cliente',
-    #'stratum.id']]
-#
-    #test = df
-    #test['stratum.id'] = test['stratum.id'].replace(100,1)
-    #test['client.type'] = test['client.type'].replace('BUILDER','BROKER')
-    #test = test[test['segmentation.habitaciones'] != 'Sin especificar']
-    #test = test[test['age.name'] != 'Sin especificar']
-    #test = test[test['baths.name'] != 'Sin especificar']
-    #test = test[test['rooms.name'] != 'Sin especificar']
-    #test = test[test['segmentation.baños'] != 'Sin especificar']
-    #test = test[test['segmentation.habitaciones'] != 'UNDEFINED']
-
-    return str(a)
-
 # %%
