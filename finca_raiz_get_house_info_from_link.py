b27c278fdb103308f6c8c7a5333555bd28a9e20c
salomonnovoam <52713477+salomonnovoam@users.noreply.github.com>
2023-05-31 21:20:12 -0500

    Update finca_raiz_get_house_info_from_link.py

--- finca_raiz_get_house_info_from_link.py
+++ finca_raiz_get_house_info_from_link.py
@@ -59,3 +59,83 @@ def get_json_from_url(link):
         print(link)
         return e
 # %%
+
+
+def prepare (i):
+    try:
+        cat_sec = i['categories']['sector']
+        # print(cat_sec)
+        for j in cat_sec:
+            value = j['slug']
+            print(value)
+            i[value]=1
+    except:
+        print('a')
+        pass
+
+    try:
+        cat_int = i['categories']['interior']
+        # print(cat_int)
+        for j in cat_int:
+            value = j['slug']
+            print(value)
+            i[value]=1
+    except:
+        print('b')
+        pass
+        
+    try:
+        cat_ext = i['categories']['exterior']
+        # print(cat_ext)
+        for j in cat_ext:
+            value = j['slug']
+            print(value)
+            i[value]=1
+    except:
+        print('c')
+        pass
+
+    return i
+
+
+def get_processed_info(link):
+
+    a = get_json_from_url(link)
+
+    a_ = prepare (a)
+
+    a_
+
+    df = pd.json_normalize(a_)[[ 'price',
+    'administration.price',
+    'age.id',
+    'age.name',
+    'area',
+    'baths.id',
+    'baths.name',
+    'client.type',
+    'condition.id',
+    'floor.id',
+    'garages.id',
+    'locations.lat',
+    'locations.lng',
+    'rooms.name',
+    'segmentation.baños',
+    'segmentation.habitaciones',
+    'segmentation.tipo_cliente',
+    'stratum.id']]
+
+    test = df
+    test['stratum.id'] = test['stratum.id'].replace(100,1)
+    test['client.type'] = test['client.type'].replace('BUILDER','BROKER')
+    test = test[test['segmentation.habitaciones'] != 'Sin especificar']
+    test = test[test['age.name'] != 'Sin especificar']
+    test = test[test['baths.name'] != 'Sin especificar']
+    test = test[test['rooms.name'] != 'Sin especificar']
+    test = test[test['segmentation.baños'] != 'Sin especificar']
+    test = test[test['segmentation.habitaciones'] != 'UNDEFINED']
+
+    return test
+
+# %%
+
